34f8f8d32a2fe385b3264dae2eccb59962504c62
Darren <nardol.darren@gmail.com>
2025-03-04 18:54:49 +0100

    UPDATE police

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,62 +1,35 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-  <!-- Primary Navigation Menu -->
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="flex justify-between h-16">
-      <div class="flex">
-        <!-- Logo -->
-        <div class="shrink-0 flex items-center">
-          <a href="{{ route('home') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-          </a>
-        </div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-        <!-- Navigation Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-          <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-nav-link>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-          <x-nav-link :href="route('login')">
-            {{ __('Log in') }}
-          </x-nav-link>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-          <x-nav-link :href="route('register')">
-            {{ __('Register') }}
-          </x-nav-link>
-        </div>
-      </div>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-
-
-      <!-- Hamburger -->
-      <div class="-me-2 flex items-center sm:hidden">
-        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-    </div>
-  </div>
-
-  <!-- Responsive Navigation Menu -->
-  <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-    <div class="pt-2 pb-3 space-y-1">
-      <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-        {{ __('Dashboard') }}
-      </x-responsive-nav-link>
-
-      <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
-        {{ __('Log in') }}
-      </x-responsive-nav-link>
-
-      <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
-        {{ __('Register') }}
-      </x-responsive-nav-link>
+<body class="font-sans text-gray-900 antialiased">
+  <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div>
+      <a href="/">
+        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+      </a>
     </div>
 
-
+    @isset($slot)
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+      {{ $slot }}
+    </div>
+    @endisset
   </div>
-</nav>
+</body>
+
+</html>
